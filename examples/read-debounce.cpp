9f3263683f4de6329942cc1e3c6373fef96070d5
thiago <thiago@tvidal.uk>
2023-10-27 18:36:18 +0100

    read-debounce example

---
+++ examples/read-debounce.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+#include "Encoder.hpp"
+
+#define DEBOUNCE_TIME   55
+
+Encoder e(PinName::p18, PinName::p19, PinMode::PullUp);
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("\n\n\nencoder-read");
+}
+
+void loop() {
+  int8_t ticks = e.ReadAndReset(DEBOUNCE_TIME);
+  if (ticks) {
+    Serial.print("ticks=");
+    Serial.println(ticks);
+  }
+}
