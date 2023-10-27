12b756999dbe23ef2fc77a9be8b126272ab32af0
thiago <thiago@tvidal.uk>
2023-10-27 18:34:01 +0100

    simple read example

---
+++ examples/read.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#include "Encoder.hpp"
+
+Encoder e(PinName::p18, PinName::p19);
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("\n\n\nencoder-read");
+}
+
+void loop() {
+  int8_t ticks = e.Read();
+  if (ticks) {
+    Serial.print("ticks=");
+    Serial.println(ticks);
+    e.Reset();
+  }
+}
